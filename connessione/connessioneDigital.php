22531fea4202dfa275c586e52591c13d29c806df
Rosario Carvello <rosario.carvello@gmail.com>
2024-03-20 09:58:56 +0100

    Add files via upload
    .

---
+++ connessione/connessioneDigital.php
@@ -0,0 +1,44 @@
+<?php
+
+class connessioneDigital {
+
+    protected $link;
+    private $_stato = false;
+    private $_host = 'siscall-digital';
+    private $_port = '3306';
+    private $_pwd = 'pass';
+    private $_userDB = 'root';
+    private $_nomedb = 'asterisk';
+
+    public function apriConnessioneDigital() {
+        $this->link = mysqli_connect($this->_host, $this->_userDB, $this->_pwd);
+        if (mysqli_connect_errno()) {
+            die("Errore nella connessione:" . mysqli_connect_errno());
+            exit();
+        } else {
+            $this->_stato = true;
+            mysqli_select_db($this->link, $this->_nomedb) or die("Errore nella selezione del database");
+            return $this->link;
+        }
+    }
+
+    function chiudiConnessioneDigital() {
+        $this->_stato = false;
+        mysqli_close($this->link);
+    }
+
+    public function getLink() {
+        return $this->link;
+    }
+
+    public function getStato() {
+        return $this->_stato;
+    }
+
+    public function __toString() {
+        return $this->_stato;
+    }
+
+}
+
+?>
