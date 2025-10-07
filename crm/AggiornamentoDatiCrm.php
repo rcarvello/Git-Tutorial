25795f0ca48c90d994984e698b88349419478937
Rosario Carvello <rosario.carvello@gmail.com>
2025-10-07 08:48:19 +0200

    Delete crm directory
    ,

--- crm/AggiornamentoDatiCrm.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-include "/var/www/html/Know/crm/enelOut_1.php";
-include "/var/www/html/Know/crm/greennetwork_1.php";
-include "/var/www/html/Know/crm/plenitude_1.php";
-include "/var/www/html/Know/crm/vivigas_1.php";
-include "/var/www/html/Know/crm/vodafone_1.php";
